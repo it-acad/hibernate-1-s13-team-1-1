45aa557b99647cdd1988eaca244988f48220c306
bohdanpodzihun <70894120+bohdanpodzihun@users.noreply.github.com>
2020-10-23 14:56:48 +0300

    Update ToDo.java

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -7,10 +7,95 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Set;
 
-
+@Entity
+@Table(name = "todos")
 public class ToDo {
-//TODO
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "todo_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "10"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+
+    private long id;
+
+    @NotBlank(message = "The todoTitle cannot be empty")
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @NotBlank(message = "The todoOwner_id cannot be empty")
+    @Column(nullable = false, unique = true)
+    private long owner_id;
+
+    @OneToMany(mappedBy = "todo")
+    private List<Task> tasks;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private List<User> users;
+
+    public ToDo() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(long owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo {" +
+                "id = " + id +
+                ", title = '" + title +
+                ", owner_id = '" + owner_id + '\'' +
+                "} ";
+    }
+
+    public OffsetDateTime getCreated_at() {
+        return null;
+    }
 }
