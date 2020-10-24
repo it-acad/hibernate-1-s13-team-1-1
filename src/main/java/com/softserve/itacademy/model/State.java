0486f25456a99675ec024e297d8301ec64b4cd0b
marichkakohut <70893890+marichkakohut@users.noreply.github.com>
2020-10-24 22:23:00 +0300

    Update State.java

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,1 +1,67 @@
+package com.softserve.itacademy.model;
 
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "states")
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id", insertable = false, updatable = false)
+    private long id;
+
+    @NotBlank(message = "The stateName cannot be empty")
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "states_id", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
+    public State() {
+    }
+
+    public State(long id, @NotBlank(message = "The stateName cannot be empty") String name, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.tasks = tasks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "State {" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                "} ";
+    }
+}
